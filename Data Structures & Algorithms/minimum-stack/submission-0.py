0d29710b05b6dbb2cd822f61ad827e0fa366bf83
Maciej Szubiczuk <106101693+maciekSzubiczuk@users.noreply.github.com>
2026-05-12 15:47:53 +0200

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,17 @@
+class MinStack:
+
+    def __init__(self):
+        self.arr = []
+
+    def push(self, val: int) -> None:
+        return self.arr.append(val)
+
+    def pop(self) -> None:
+        return self.arr.pop()
+
+    def top(self) -> int:
+        return self.arr[-1]
+
+    def getMin(self) -> int:
+        return min(self.arr)
+        
